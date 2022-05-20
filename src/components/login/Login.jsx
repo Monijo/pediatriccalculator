93e38303623278b9e57071540282407b3e2d656b
Monijo <mokajozefowicz@gmail.com>
2022-05-20 19:56:35 +0200

    html and css for login component.

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import {Link} from "react-router-dom";
+import "./login.css"
+
+
+function Login(props) {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    return (
+        <div className="login-box">
+            <h1>Zaloguj się</h1>
+            <input type="text"
+                   placeholder="Email"
+                   value={email}
+                   onChange={(e)=>setEmail(e.target.value)}/>
+            <input type="password"
+                   placeholder="Hasło"
+                   value={password}
+                   onChange={(e)=>setPassword(e.target.value)}/>
+            <p>Nie masz konta? <Link to="/">Zarejestruj się</Link></p>
+            <button>Zaloguj się</button>
+            <p>
+
+            <Link to="/">Kontynuuj bez rejestracji</Link>
+
+            </p>
+        </div>
+    );
+}
+
+export default Login;
