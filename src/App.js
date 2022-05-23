d467bab359376cd87071f44d75a4cf6e05d7cba7
Monijo <mokajozefowicz@gmail.com>
2022-05-23 11:09:21 +0200

    Add Home view and NavBar component.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import {
 import './App.css';
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
+import AddChild from "./components/addChild/AddChild";
+import Home from "./components/home/Home";
 
 
 
@@ -14,8 +16,10 @@ function App() {
   return (
       <BrowserRouter>
         <Routes>
+            <Route path="/" element={<Home/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/register" element={<Register/>}/>
+            <Route path="/addchild" element={<AddChild/>}/>
 
 
 
